9f1eb074c5e4bdaa6983bc15afa75de7e10a5d58
instrutor <instrutor@ceplabmacstudio01.local>
2024-02-17 09:27:50 -0300

    atualização pós PI

---
+++ View/cardapio.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style_cardapio.css" />  
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <div class= "ModalOverlay">
+      <div class= "ModalContent">
+            <h3 class= "ModalTitle">CARDÁPIO</h4>
+            <h4 class="ModalDescription"></h4>
+            <button class="CloseButton" onClick=Close()>Fechar</CloseButton>
+       </div>
+    </div>
+
+
+
+</body>
+</html>
